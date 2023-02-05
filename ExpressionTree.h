8846877b8d54fa8413b72c9fdebdd6ad4c85a2d4
Wreckeshey <RTSmith801@gmail.com>
2023-02-04 20:54:43 -0700

    first commit

---
+++ ExpressionTree.h
@@ -0,0 +1,13 @@
+/**
+ * @file: ExpressionTree.h
+ * @assignment_name: 06: Try it out: Expression Tree
+ * @author: Rick Smith (rickysmith@mail.weber.edu)
+ * @date: 02/04/2023
+ * @brief: Implementing a BinaryTree to store math expressions.
+ *
+ */
+
+#ifndef INC_06_TRY_IT_OUT_EXPRESSION_TREE_EXPRESSIONTREE_H
+#define INC_06_TRY_IT_OUT_EXPRESSION_TREE_EXPRESSIONTREE_H
+
+#endif //INC_06_TRY_IT_OUT_EXPRESSION_TREE_EXPRESSIONTREE_H
